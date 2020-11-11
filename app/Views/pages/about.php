d29814ac83ed3fc913077ff83e32f20d3d188254
IvoArsov <arsov.ivaylo@gmail.com>
2020-11-11 15:03:48 +0200

    First Commit

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<section>
+    <div class="container">
+        <h1>About</h1>
+        <p>
+            <div class="500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tempora animi voluptate maxime unde fugiat amet modi eius ad nemo quasi, commodi beatae iure ullam cupiditate, molestiae odit non facilis.</div>
+        </p>
+    </div>
+</section>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,15 @@
+<section>
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-4">Code Ignater Blog demo</h1>
+            <p class="lead">
+                <div class="600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptates quia at quam itaque delectus quaerat sed ullam mollitia fugit dolor, eius rerum, ipsum earum harum. Quaerat labore excepturi autem.</div>
+            </p>
+            <hr class="my-4">
+            <p>
+                <div class="10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sunt id voluptates eum, repudiandae ratione! Ad earum cupiditate omnis enim amet velit, optio dicta nesciunt incidunt libero, doloribus nulla vitae.</div>
+            </p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+    </div>
+</section>
