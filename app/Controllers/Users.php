39fbc9faa91c222f4bd1ae51aff200ae87281a23
IvoArsov <arsov.ivaylo@gmail.com>
2020-11-18 11:54:46 +0200

    small changes

---
+++ app/Controllers/Users.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+use App\Models\BlogModel;
+
+class Users extends BaseController
+{
+	public function index()
+	{
+        echo 'test';
+    }
+
+	//--------------------------------------------------------------------
+
+}
